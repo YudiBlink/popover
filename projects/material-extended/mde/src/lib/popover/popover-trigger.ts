fb504c26e05e8206ff1d85d185c6f1f04916bbf6
STS\kiet.truongh <kiet.truongh@saigontechnology.com>
2023-09-18 13:10:32 +0700

    upgrade to 16

--- projects/material-extended/mde/src/lib/popover/popover-trigger.ts
+++ projects/material-extended/mde/src/lib/popover/popover-trigger.ts
@@ -250,7 +250,6 @@ export class MdePopoverTrigger implements AfterViewInit, OnDestroy { // tslint:d
 
     /** Closes the popover. */
     closePopover(): void {
-      console.log('close');
         if (this._overlayRef) {
           this._overlayRef.detach();
           this._resetPopover();
@@ -412,7 +411,6 @@ export class MdePopoverTrigger implements AfterViewInit, OnDestroy { // tslint:d
     private _getOverlayScrollStrategy(strategy: MdePopoverScrollStrategy): ScrollStrategy {
       switch(strategy) {
         case 'noop':
-          console.log('noop');
           return this._overlay.scrollStrategies.noop();
         case 'close':
           return this._overlay.scrollStrategies.close();
